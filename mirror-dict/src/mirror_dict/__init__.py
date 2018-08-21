fbdeb5c8a650e537094d49674b277c7e913fe8e8
Jacob Hayes <jacob.r.hayes@gmail.com>
2018-08-21 17:00:37 -0500

    mirror-dict: Move to (poetry) package, use __slots__, and fix unintended . access from __dict__

--- mirror-dict/src/mirror_dict/__init__.py
+++ mirror-dict/src/mirror_dict/__init__.py
@@ -1,18 +1,25 @@
 """ MirrorDict provides a mapping that returns the key if no value is found.
 """
-from collections import Mapping
+from collections.abc import Mapping
+
 
 class MirrorDict(Mapping):
     """ Mapping that returns the key if no value is found.
     """
+
+    __slots__ = ("_store",)
+
     def __init__(self, *args, **kwargs):
-        self.__dict__.update(dict(*args, **kwargs))
+        self._store = dict(*args, **kwargs)
 
     def __getitem__(self, key):
-        return self.__dict__.get(key, key)
+        return self._store.get(key, key)
 
     def __iter__(self):
-        return iter(self.__dict__)
+        return iter(self._store)
 
     def __len__(self):
-        return len(self.__dict__)
+        return len(self._store)
+
+    def __repr__(self):
+        return f"{type(self)}({self._store})"
